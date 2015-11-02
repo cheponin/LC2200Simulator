a398f55f7b4d83babbd9fa35d521a879a72dba6f
cheponin <nina.chepovska@gmail.com>
2015-11-02 15:26:48 -0800

    Created test input file and assembler now able to read files

--- assembler.c
+++ assembler.c
@@ -6,15 +6,25 @@
 #include <stdlib.h>
 
 int main() {
-   char instruction[18] = "add $v0, $a0, $a1";
-   char opcode[4];
+
+   FILE *inputFile = fopen("input.txt", "r");
+   char opcode[5];
    char operand1[6];
    char operand2[6];
    char operand3[6];
    
-   sscanf(instruction, "%s %s %s %s", opcode, operand1, operand2, operand3);
+   fgets(opcode, 5, inputFile);
+   //fgetc(inputFile);
+   fgets(operand1, 4, inputFile);
+   fgetc(inputFile);
+   fgetc(inputFile);
+   fgets(operand2, 4, inputFile);
+   fgetc(inputFile);
+   fgetc(inputFile);
+   fgets(operand3, 4, inputFile);
 
    printf("Opcode: %s\nOperand 1: %s\nOperand 2: %s\nOperand 3: %s\n", opcode, operand1, operand2, operand3);
+   fclose(inputFile);
 
    return 0;
 }
